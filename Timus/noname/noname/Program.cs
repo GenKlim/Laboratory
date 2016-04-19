318de18d8623bf6c0183eb4c52ebb344ca2d00b7
mrkriv <virus_450@mail.ru>
2016-04-19 21:42:26 +0300

    add lab

---
+++ Timus/noname/noname/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество напитков:");
+        int count = int.Parse(Console.ReadLine());
+        int result = 0;
+        for (int i = 2; i <= count; i++)
+            result += A(count, i);
+
+        Console.WriteLine("Можно составить {0} различных коктейлей.", result);
+        Console.ReadKey();
+    }
+
+    static int Factorial(int x)
+    {
+        if (x <= 1)
+            return 1;
+        else
+            return x * Factorial(x - 1);
+    }
+
+    static int A(int n, int k)
+    {
+        return Factorial(n) / Factorial(n - k);
+    }
+}
