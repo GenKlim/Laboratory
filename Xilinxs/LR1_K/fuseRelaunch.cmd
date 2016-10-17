541d2822e1e436c072cc2307288fe6f20fe5af89
mrkriv <virus_450@mail.ru>
2016-10-17 11:51:10 +0300

    rename dirs and add Leha pr1

--- Xilinxs/LR1_K/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/admin/Documents/Visual Studio 2015/Projects/Laboratory/Xilinxs/LR1_K/Test_top_isim_beh.exe" -prj "C:/Users/admin/Documents/Visual Studio 2015/Projects/Laboratory/Xilinxs/LR1_K/Test_top_beh.prj" "work.Test_top" "work.glbl" 
---
+++ Xilinxs/PR1/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/admin/Documents/Visual Studio 2015/Projects/Laboratory/Xilinxs/LR1/LR1_TB_isim_beh.exe" -prj "C:/Users/admin/Documents/Visual Studio 2015/Projects/Laboratory/Xilinxs/LR1/LR1_TB_beh.prj" "work.LR1_TB" "work.glbl" 
