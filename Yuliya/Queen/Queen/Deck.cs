d09bf3492a178f90325c2215970aee2740d69236
mrkriv <virus_450@mail.ru>
2016-12-11 16:24:06 +0300

    Add task X queen

---
+++ Yuliya/Queen/Queen/Deck.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queen
+{
+    /// <summary>
+    /// Класс шахмотной доски
+    /// </summary>
+    public class Deck
+    {
+        /// <summary>
+        /// Размер доски
+        /// </summary>
+        public readonly int Size = 0;
+
+        /// <summary>
+        /// Двумерный массив ячеек доски
+        /// </summary>
+        private Cell[,] map;
+
+        /// <summary>
+        /// Ячейка шахмотной доски
+        /// </summary>
+        private enum Cell : byte
+        {
+            Clear,  // Пустая
+            Queen,  // Ферзь
+            Attack, // Клетка, которую может аттаковать квина
+        }
+
+        /// <summary>
+        /// Конструктор, создает доску заданного размера
+        /// </summary>
+        /// <param name="size">Размер доски</param>
+        public Deck(int size)
+        {
+            map = new Cell[size, size];
+            Size = size;
+        }
+
+        /// <summary>
+        /// Коеструктор, корирует доску
+        /// </summary>
+        /// <param name="master">Доска, которую копируем</param>
+        public Deck(Deck master)
+        {
+            map = (Cell[,])master.map.Clone();  //Копируем двумерный массив
+            Size = master.Size;
+        }
+
+        /// <summary>
+        /// Проверяет что в ячейки нет квины и ни одна квина не может атаковать эту ячейку
+        /// </summary>
+        public bool IsClear(int x, int y)
+        {
+            return map[x, y] == Cell.Clear;
+        }
+
+        /// <summary>
+        /// Проверяет есть ли свободное место на доске
+        /// </summary>
+        public bool IsFull()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (map[i, j] == Cell.Clear)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Ставит ферзя на по зпаданным координатам и отмечает клетки которые она блокирует
+        /// </summary>
+        public void SetQueen(int x, int y)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                //Рисуем плюсик
+                map[i, y] = Cell.Attack;
+                map[x, i] = Cell.Attack;
+
+                //Рисуем крестик...
+                if (x + i < Size == y + i < Size)
+                    map[(x + i) % Size, (y + i) % Size] = Cell.Attack;
+
+                if (x + i < Size != y + Size - i < Size)
+                    map[(x + i) % Size, (y + Size - i) % Size] = Cell.Attack;
+            }
+
+            map[x, y] = Cell.Queen; //Ставим квину
+        }
+
+        /// <summary>
+        /// Рисует доску, нужно только для вывода в консоль
+        /// </summary>
+        public override string ToString()
+        {
+            string str = "";
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                    str += CellToString(map[i, j]) + " ";
+
+                str += "\n";
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Сопостовляет содержимому ячейки символ
+        /// </summary>
+        /// <param name="cell">Ячейка шахмотной доски</param>
+        private string CellToString(Cell cell)
+        {
+            switch (cell)
+            {
+                case Cell.Queen: return "O";
+                case Cell.Attack: return "-";
+                default: return " ";
+            }
+        }
+    }
+}
---
+++ Yuliya/Queen/Queen/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queen
+{
+    class Program
+    {
+        /// <summary>
+        /// Размер шахмотной доски
+        /// </summary>
+        private const int Size = 8;
+
+        static void Main(string[] args)
+        {
+            DateTime startTime = DateTime.Now;  // Засекаем время
+
+            Console.WriteLine("Начинаю перебор");
+            int count = 1;
+
+            // Увеличивает количество квин, пока не найдет решение
+            while (!Tray(count))
+                count++;
+
+            Console.WriteLine("Перебор окончен, затраченно {0} секунд", (DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine("Для блокирования шахмотной доски {0}x{0} понадробится минимум {1} ферзей", Size, count);
+            Console.ReadKey();
+        }
+        
+        /// <summary>
+        /// Пробует заблокировать доску заданным кличеством квин
+        /// </summary>
+        /// <param name="count">Количество квин</param>
+        private static bool Tray(int count)
+        {
+            Console.WriteLine("Пробую разместить {0} ферзей", count);
+            return Set(count, new Deck(Size));
+        }
+
+        /// <summary>
+        /// Рекурсивный метод, ищет место для заданного количества квин на доске
+        /// Вся магия здесь
+        /// </summary>
+        /// <param name="count">Сколько еще нужно квин</param>
+        /// <param name="master">Доска на которой размещаем</param>
+        /// <returns></returns>
+        private static bool Set(int count, Deck master)
+        {
+            if (count == 0) // Если квин не осталось, то проверим есть ли свободные места
+                return master.IsFull();
+
+            // Перебирает все комбинации на доске
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (master.IsClear(i, j))   // Если клетка свободна, ставим квину на копию доски
+                    {
+                        Deck deck = new Deck(master);   // Копируем доску, что бы не портить основную
+                        deck.SetQueen(i, j);            // Ставим квину на копию доски
+
+                        //Console.WriteLine(deck);      // Показывает как происходит перебор (Вывод в консоль сильно тормозит программу)
+
+                        if (Set(count - 1, deck))       // Рекурсия, пытаемся поставить на копию доски еще count-1 квин
+                        {
+                            // Если разместили всех квин и не осталось свободных ячеек
+                            Console.WriteLine(deck);    // Показывает результат (В обратном порядке)
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
